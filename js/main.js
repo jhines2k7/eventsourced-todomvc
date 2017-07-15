a4e07e79a8476d7cc8bc49072524005629d8bca0
James Hines <jameshines10@gmail.com>
2017-07-15 11:46:25 -0400

    Adds working toggle

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ import reduce from './reducer'
 import EventStore from './eventStore'
 import postal from 'postal/lib/postal.lodash'
 
-document.querySelector('.new-todo').addEventListener('keypress', function (e) {
+document.querySelector('.new-todo').addEventListener('keypress', (e) => {
     let key = e.which || e.keyCode;
     
     if (key === 13) {
@@ -49,6 +49,30 @@ postal.subscribe({
     }.bind(this)
 });
 
+postal.subscribe({
+    channel: 'async',
+    topic: 'todo.toggle',
+    callback: function(data, envelope) {
+        let state = reduce(EventStore.events);
+
+        let footer = document.querySelector('.footer');
+        let label = document.querySelector('label[for="toggle-all"]');
+        let input = document.querySelector('.toggle-all');
+
+        if(state.todos.length > 0) {
+            footer.style.display = 'block';
+            label.style.display = 'block';
+            input.style.display = 'block';
+            renderTodos(state);    
+        } else {
+            footer.style.display = 'none'; 
+            label.style.display = 'none'; 
+            input.style.display = 'none'; 
+        }
+        
+    }.bind(this)
+});
+
 function renderTodos(state) {
     let ul = document.querySelector('.todo-list');
 
@@ -62,17 +86,34 @@ function renderTodos(state) {
         ul.removeChild(ul.firstChild);
     }
 
-    state.todos.forEach((todo) => {
+    state.todos.forEach((todo, idx) => {
         let fragment = document.createDocumentFragment();
 
         let li = document.createElement('li');
-        
+        li.setAttribute('data-idx', idx);
+        li.setAttribute('data-complete', todo.complete);
+
         let div = document.createElement('div');
         div.className = 'view';
         
         let input = document.createElement('input');
         input.className = 'toggle'
         input.setAttribute('type', 'checkbox');
+
+        if(todo.complete) {
+            li.className = 'completed';
+            input.checked = true;
+        }
+        
+        input.addEventListener('click', (e) => {
+            let parentLi = e.currentTarget.closest('li');
+            
+            EventStore.add(EventStore.events, [{
+                channel: 'async',
+                topic: 'todo.toggle',
+                data: idx
+            }]);
+        });        
         
         let label = document.createElement('label');
         label.textContent = todo.label;
--- js/reducer.js
+++ js/reducer.js
@@ -1,17 +1,29 @@
 export default function reduce(events) {
     "use strict";
 
+    let toggleState = {};
+
+    /* 
+    toggleState = {
+        0: true,
+        2: false,
+        3: false
+    }
+    */
+
     return events.reduce( (state, event) => {
-        if(event.topic === 'todo.add') {
-            let todos = state.todos.slice();
-            
-            todos.splice(todos.length, 0, event.data)
-            
-            state.todos = todos;
+        if(event.topic === 'todo.add') {            
+            state.todos.push(event.data);
         }
 
         if(event.topic === 'increment-item-count') {
             state.itemsLeft++;
+        }
+
+        if(event.topic === 'todo.toggle') {
+            toggleState[event.data] = !toggleState[event.data];
+
+            state.todos[event.data].complete =  toggleState[event.data];
         }
 
         return state;
