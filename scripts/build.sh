2fedb5dbb079eb9450544d799b807e6e58777b84
James Hines <jameshines10@gmail.com>
2017-07-19 13:28:44 -0400

    Updates the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,6 +21,14 @@ node_modules/.bin/webpack --config=webpack.config.js
 # copy index.html to dist directory
 cp index.html dist
 
+# copy css files to dist directory
+cp node_modules/todomvc-app-css/index.css dist
+cp node_modules/todomvc-common/base.css dist
+
+# update the css refs in index.html
+sed -i 's/node_modules\/todomvc-common\/base.css/base.css/g' dist/index.html
+sed -i 's/node_modules\/todomvc-app-css\/index.css/index.css/g' dist/index.html
+
 # change the src property of the script tag to bundle.js
 sed -i 's/dist\/bundle.js/bundle.js/g' dist/index.html
 
